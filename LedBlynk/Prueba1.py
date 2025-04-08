77a54ee768938438ac110a9f6743f9118c560435
Max R <maxr2123@gmail.com>
2025-04-08 11:04:42 -0600

    feat: add initial Blynk LED control script
    This commit introduces a new Python script to control an LED using the Blynk platform. The script initializes a Blynk connection, configures a GPIO pin for the LED, and sets up a virtual write handler to toggle the LED based on Blynk app input. The script also includes cleanup logic for graceful shutdown on keyboard interrupt.

---
+++ LedBlynk/Prueba1.py
@@ -0,0 +1,35 @@
+import gpiod
+import time
+import BlynkLib
+
+# Configura tu Auth Token aquí
+BLYNK_AUTH = 'LLl4IaHQoLIHU_4qe8_9SuBrEcz3WGQ1'
+
+# Inicializa Blynk
+blynk = BlynkLib.Blynk(BLYNK_AUTH)
+
+# Configura el pin GPIO
+LED_PIN = 18
+
+chip = gpiod.Chip('gpiochip4')
+led_line = chip.get_line(LED_PIN)
+led_line.request(consumer='LED', type=gpiod.LINE_REQ_DIR_OUT)
+
+# Cuando cambia el valor del botón en Blynk (por ejemplo en V0)
+@blynk.VIRTUAL_WRITE(0)
+def v0_write_handler(value):
+    if int(value[0]) == 1:
+        led_line.set_value(1)
+    else:
+        led_line.set_value(0)
+
+try:
+    while True:
+        blynk.run()
+        time.sleep(0.1)  # Small delay to prevent CPU overuse
+except KeyboardInterrupt:
+    # Clean up when the user interrupts the script
+    led_line.release()
+    chip.close()
+    
+    # :)
