cb5e92e939757762d6c88fc800af44d9aca3f53b
Max R <maxr2123@gmail.com>
2025-05-15 18:04:23 -0600

    Update LDR

--- LedBlynk/PruebaaLuzBlynk.py
+++ LedBlynk/PruebaaLuzBlynk.py
@@ -1,25 +1,24 @@
+## Prueba de Blynk con LDR (sin MCP3008)
+import BlynkLib
 import RPi.GPIO as GPIO
 import time
-from BlynkLib import Blynk
 
-BLYNK_AUTH = 'TU_TOKEN_BLYNK'
+BLYNK_AUTH_TOKEN = "5biBY2fS84cyCHbeU_lgTTEe4EFz-DAY"
+blynk = BlynkLib.Blynk(BLYNK_AUTH_TOKEN, server='blynk.cloud', port=80)
 
-# Configura Blynk
-blynk = Blynk(BLYNK_AUTH)
+LDR_PIN = 7  # Usamos el mismo pin que en tu ejemplo
 
-# Configura GPIO
-LDR_PIN = 18
 GPIO.setmode(GPIO.BCM)
 
 def rc_time(pin):
     count = 0
 
-    # Cargar el capacitor
+    # Cargar el condensador
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, False)
     time.sleep(0.1)
 
-    # Medir el tiempo de descarga
+    # Medir el tiempo de descarga (mientras el pin esté en bajo)
     GPIO.setup(pin, GPIO.IN)
     while GPIO.input(pin) == GPIO.LOW:
         count += 1
@@ -28,9 +27,14 @@ def rc_time(pin):
 try:
     while True:
         ldr_value = rc_time(LDR_PIN)
-        print("LDR:", ldr_value)
-        blynk.virtual_write(0, ldr_value)
+        print(f"LDR Value: {ldr_value}")
+
+        # Enviar a Blynk en V0
+        blynk.virtual_write(1, ldr_value)
+
         blynk.run()
         time.sleep(1)
+
 except KeyboardInterrupt:
+    print("Programa interrumpido por el usuario")
     GPIO.cleanup()
